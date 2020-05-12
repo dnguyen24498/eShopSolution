b08b7af4a57565af7367fb9ceb3784d4967adc0f
dnguyen24498 <62144517+dnguyen24498@users.noreply.github.com>
2020-05-12 15:27:04 +0700

    -Add seeding example data

--- eShopSolution.Data/EF/EShopSolutionDbContext.cs
+++ eShopSolution.Data/EF/EShopSolutionDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using eShopSolution.Data.Configurations;
 using eShopSolution.Data.Entities;
+using eShopSolution.Data.Extensions;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -13,6 +14,7 @@ namespace eShopSolution.Data.EF
         public EShopSolutionDbContext(DbContextOptions<EShopSolutionDbContext> options) : base(options){}
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //Configure using Fluent API
             modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
@@ -27,6 +29,9 @@ namespace eShopSolution.Data.EF
             modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
             modelBuilder.ApplyConfiguration(new PromotionConfiguration());
             modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+
+            //Data seeding
+            modelBuilder.Seeding();
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
