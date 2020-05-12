94204d69fd77fc7eb5fb83e85b84473d129477a7
dnguyen24498 <62144517+dnguyen24498@users.noreply.github.com>
2020-05-12 09:11:29 +0700

    -Add Entity Framework Core -Add Fluent Configuration -Migration Database -Create Database

---
+++ eShopSolution.Data/EF/EShopSolutionContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopSolutionContextFactory : IDesignTimeDbContextFactory<EShopSolutionDbContext>
+    {
+        public EShopSolutionDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+            var connectionstring = configuration.GetConnectionString("eShopSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<EShopSolutionDbContext>();
+            optionsBuilder.UseSqlServer(connectionstring);
+            return new EShopSolutionDbContext(optionsBuilder.Options);
+        }
+    }
+}
