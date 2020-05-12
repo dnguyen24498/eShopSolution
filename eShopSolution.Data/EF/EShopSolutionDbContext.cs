2722a7a2d1e9eb1909c73708a30f6287800ea995
dnguyen24498 <62144517+dnguyen24498@users.noreply.github.com>
2020-05-12 21:41:33 +0700

    Update EShopSolutionDbContext.cs
    -Edit configuration for Identity

--- eShopSolution.Data/EF/EShopSolutionDbContext.cs
+++ eShopSolution.Data/EF/EShopSolutionDbContext.cs
@@ -29,6 +29,14 @@ namespace eShopSolution.Data.EF
             modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
             modelBuilder.ApplyConfiguration(new PromotionConfiguration());
             modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+            modelBuilder.ApplyConfiguration(new AppUserConfiguration());
+            modelBuilder.ApplyConfiguration(new AppRoleConfiguration());
+
+            modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("AppUserClaims");
+            modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("AppUserRoles").HasKey(x=>new {x.UserId,x.RoleId});
+            modelBuilder.Entity<IdentityUserLogin<Guid>>().ToTable("AppUserLogins").HasKey(x=>x.UserId);
+            modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("AppRoleClaims");
+            modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("AppUserTokens").HasKey(x=>x.UserId);
 
             //Data seeding
             modelBuilder.Seeding();
