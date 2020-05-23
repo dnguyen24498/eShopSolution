c0097d110c315122523ac281aef29bb645a21b50
dnguyen24498 <62144517+dnguyen24498@users.noreply.github.com>
2020-05-23 17:14:08 +0700

    -Add Login, Logout features

---
+++ eShopSolution.AdminApp/Services/IUserApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using eShopSolution.ViewModels.Common;
+using eShopSolution.ViewModels.System.Users;
+
+namespace eShopSolution.AdminApp.Services
+{
+    public interface IUserApiClient
+    {
+        Task<ApiResult<string>> Login(LoginUserRequest request);
+
+        Task<ApiResult<bool>> Register(RegisterUserRequest request);
+    }
+}
---
+++ eShopSolution.AdminApp/Services/UserApiClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using eShopSolution.ViewModels.Common;
+using eShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace eShopSolution.AdminApp.Services
+{
+    public class UserApiClient:IUserApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserApiClient(IHttpClientFactory clientFactory, IConfiguration configuration,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClientFactory = clientFactory;
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public async Task<ApiResult<string>> Login(LoginUserRequest request)
+        {
+            var json = JsonConvert.SerializeObject(request);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_configuration["BaseAddress"]);
+            var response = await client.PostAsync("/api/Users/login", httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return JsonConvert.DeserializeObject<ApiSuccessResult<string>>(await response.Content.ReadAsStringAsync());
+            }
+
+            return JsonConvert.DeserializeObject<ApiErrorResult<string>>(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<ApiResult<bool>> Register(RegisterUserRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
